069f863f292fe07c027c0ba9fbe9b0d710169cfd
chuong2k8 <70183831+chuong2k8@users.noreply.github.com>
2020-08-25 09:06:32 +0700

    Create Introducting.cpp

---
+++ Introducting.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+using namespace std;
+
+int home() {
+  cout << "Hello! My name is Chuong!" << endl << "This is my first time I make a file in GitHub!" << endl << "Using GitHub.com and C++!";
+}
